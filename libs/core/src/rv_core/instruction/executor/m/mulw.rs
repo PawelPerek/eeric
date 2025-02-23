84d91150ccd1dcd8b5dbe4033e95f69b8b4f63d8
Pawel Perek <pawelperek99@gmail.com>
2025-02-23 17:19:38 +0100

    Fix mulw s-ext

--- libs/core/src/rv_core/instruction/executor/m/mulw.rs
+++ libs/core/src/rv_core/instruction/executor/m/mulw.rs
@@ -1,6 +1,30 @@
 use crate::rv_core::instruction::executor::prelude::*;
 
 pub fn mulw(R { rd, rs1, rs2 }: R, x: &mut IntegerRegisters) {
-    let result = (x[rs1] as u32).wrapping_mul(x[rs2] as u32);
-    x[rd] = result as u64;
+    let result = (x[rs1] as i32).wrapping_mul(x[rs2] as i32);
+    x[rd] = result as i64 as u64;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mulw_works() {
+        let mut x = IntegerRegisters::new(&Memory::default());
+
+        x[A1] = 0xb504f334;
+        x[A2] = -2i64 as u64;
+
+        mulw(
+            R {
+                rd: A0,
+                rs1: A1,
+                rs2: A2
+            },
+            &mut x
+        );
+
+        assert_eq!(x[A0], 0xffffffff95f61998);
+    }
 }
